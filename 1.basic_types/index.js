5bd2ecc041d530c3b138080826146811030e8934
Vova <vova77gh@gmail.com>
2020-02-10 19:09:07 +0200

    About basic types

---
+++ 1.basic_types/index.js
@@ -0,0 +1,7 @@
+var str = 'Hello ts';
+var num = 42;
+var isActive = false;
+var strArray = ['A', 'B', 'C'];
+var numArray = [1, 1, 2, 3];
+var strArrayAlt = ['A', 'B', 'C'];
+var numArrayAlt = [1, 1, 2, 3];
---
+++ 1.basic_types/index.ts
@@ -0,0 +1,9 @@
+let str: string = 'Hello ts'
+let num: number = 42
+let isActive: boolean = false
+
+let strArray: string[] = ['A', 'B', 'C']
+let numArray: number[] = [1, 1, 2, 3]
+
+let strArrayAlt: Array<string> = ['A', 'B', 'C']
+let numArrayAlt: Array<number> = [1, 1, 2, 3]
